a6d1e6294add7eb0b3c7e4a73d8dd147916284cd
Darshini H L <darshinihlgowda@gmail.com>
2026-03-10 18:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (8.76%) - LeetSync

--- 27-remove-element/remove-element.c
+++ 27-remove-element/remove-element.c
@@ -1,10 +1,10 @@
 int removeElement(int* nums, int numsSize, int val) {
-    int k=0;
+    int count=0;
     for(int i=0;i<numsSize;i++){
         if(nums[i]!=val){
-            nums[k]=nums[i];
-            k++;
+            nums[count]=nums[i];
+            count++;
         }
     }
-    return k;
+    return count;
 }
